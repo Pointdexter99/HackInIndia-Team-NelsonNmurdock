18dee6f6734d168ac2bc8f20bb0ae0c8b8b08968
Pointdexter99 <49776332+Pointdexter99@users.noreply.github.com>
2020-07-02 02:23:38 +0530

    Add files via upload

---
+++ HackInIndia/HackInIndia/firstpage/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def home(request):
+    return render(request, 'firstpage/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
